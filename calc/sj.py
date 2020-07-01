33972d1e6467f366691afe85f643367ac3c180f3
kmu20203059 <robin1415@kookmin.ac.kr>
2020-07-01 20:28:07 +0900

    issue #5 a b에 입력받은것이 숫자가 아닐 때의 예외 처리

--- calc/sj.py
+++ calc/sj.py
@@ -5,15 +5,18 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    x = 0
-    y = 0
-    if '' not in [a, b]:
-        a, b = int(a), int(b)
-        x = a + b
-        y = a * b
+    sum = 0
+    mul = 0
+    try:
+        a, b = int(a),int(b)  
+        sum = a +b
+        mul = a * b
+    except ValueError:
+        sum = -1
+        mul = -1
     response_body = html % {
-        'x' : x or '',
-        'y' : y or '',
+        'sum' : sum,
+        'mul' : mul 
         }
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
